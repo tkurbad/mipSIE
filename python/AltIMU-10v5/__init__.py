90d6f1df3c49f664cc5deaca521ecc849d79fe28
Torsten Kurbad <github@tk-webart.de>
2016-07-17 23:11:03 +0200

    Added __init__.py

---
+++ python/AltIMU-10v5/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+############ Python library module for Pololu's AltIMU-10v5 ############
+#
+# This module enables the Raspberry PI embedded computer to use Pololu's
+# AltIMU-10v5 Inertial Measurement Unit (IMU).
+# [https://www.pololu.com/product/2739]
+#
+# This Python code was initially derived from Pololu's C++ Arduino
+# libraries, available at [https://github.com/pololu]
+#
+# The Python code is developed and maintained by
+# Torsten Kurbad <github@tk-webart.de>
+#
+########################################################################
