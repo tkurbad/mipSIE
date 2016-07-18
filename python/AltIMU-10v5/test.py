2281a1069e19efdbf7e50aae1bc32e1afc54233a
Torsten Kurbad <github@tk-webart.de>
2016-07-18 08:54:46 +0200

    More comprehensive testing

--- python/AltIMU-10v5/test.py
+++ python/AltIMU-10v5/test.py
@@ -4,6 +4,7 @@ from time import sleep
 
 from lsm6ds33 import LSM6DS33
 from lis3mdl import LIS3MDL
+from lps25h import LPS25H
 
 imu = LSM6DS33()
 imu.enable()
@@ -11,6 +12,15 @@ imu.enable()
 magnet = LIS3MDL()
 magnet.enable()
 
+baro = LPS25H()
+baro.enable()
+
 while True:
     print imu.getTemperatureCelsius()
+    sleep(0.1)
     print magnet.getMagnetometerRaw()
+    sleep(0.1)
+    print baro.getBarometerMillibars()
+    sleep(0.1)
+    print baro.getAltitude()
+    sleep(0.1)
