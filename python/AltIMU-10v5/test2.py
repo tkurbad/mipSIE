3b39527a8471cbb031dbb633e83af4e55ae58ef3
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2016-07-18 18:05:08 +0200

    Lighten up test

--- python/AltIMU-10v5/test2.py
+++ python/AltIMU-10v5/test2.py
@@ -10,11 +10,11 @@ imu.enable()
 
 imu.calibrateGyroAngles()
 
-for x in range(1000):
-    startTime = datetime.now()
-    angles = imu.trackGyroAngles(deltaT = 0.0002)
+#for x in range(1000):
+#    startTime = datetime.now()
+#    angles = imu.trackGyroAngles(deltaT = 0.0002)
 
-print angles
+#print angles
 
 for x in range(100):
     print "Accel:", imu.getAccelerometerAngles()
