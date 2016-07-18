1c4b75b16012a7b3772b6c0f795bde60f4ffc264
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2016-07-18 13:21:28 +0200

    Profiling

--- python/AltIMU-10v5/test2.py
+++ python/AltIMU-10v5/test2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+from cProfile import Profile
+from cStringIO import StringIO
+from pstats import Stats
 from time import sleep
 
 from altimu import AltIMU
@@ -7,5 +10,16 @@ from altimu import AltIMU
 imu = AltIMU()
 imu.enable()
 
-while True:
+profile = Profile()
+
+profile.enable()
+x = 0
+while x < 2000:
     print "Gyro Angles:", imu.trackGyroAngle()
+    x += 1
+profile.disable()
+
+stream = StringIO()
+stats = Stats(profile, stream = stream).sort_stats('cumulative')
+stats.print_stats()
+print stream.getvalue()
