1cc76b7e860a490830d47b75dd2573410d255333
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2016-07-18 15:28:47 +0200

    More testing

--- python/AltIMU-10v5/test2.py
+++ python/AltIMU-10v5/test2.py
@@ -12,6 +12,6 @@ imu.calibrateGyroAngles()
 
 for x in range(50000):
     startTime = datetime.now()
-    angles = imu.trackGyroAngle(deltaT = 0.0005)
+    angles = imu.trackGyroAngle(deltaT = 0.0002)
 
 print angles
