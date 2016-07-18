d7fcb51c9d9ca30840a279c1eb4b0e001e2abaa8
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2016-07-18 13:48:09 +0200

    Try automatic correction of gyro loop time

--- python/AltIMU-10v5/test2.py
+++ python/AltIMU-10v5/test2.py
@@ -12,17 +12,23 @@ imu.enable()
 
 profile = Profile()
 
-profile.enable()
-for x in range(1000):
-    print "Gyro Angles:", imu.trackGyroAngle()
-profile.disable()
+cumTime = []
+for profileRun in range(3):
+    profile.enable()
+    for x in range(1000):
+        print "Gyro Angles:", imu.trackGyroAngle()
+    profile.disable()
 
-stream = StringIO()
-stats = Stats(profile, stream = stream).sort_stats('cumulative')
-stats.print_stats(0)
+    stream = StringIO()
+    stats = Stats(profile, stream = stream).sort_stats('cumulative')
+    stats.print_stats(0)
 
-stream.seek(0)
-looptime = float(stream.readline().split(' ')[-2]) / 1000.0
+    stream.seek(0)
+    cumTime[profileRun] = float(stream.readline().split(' ')[-2])
+
+looptime = float(sum(cumTime)) / (float(len(cumTime)) * 1000.0)
+
+print "Average loop time:", looptime
 
 imu.calibrateGyroAngles()
 
