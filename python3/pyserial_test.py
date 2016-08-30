ebf338a06f015ad7046c26767a4e6739f71142db
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2016-08-30 17:47:37 +0200

    Added PySerial test (for XRF module)

---
+++ python3/pyserial_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import serial
+
+ser = serial.Serial(
+    port='/dev/ttyAMA0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+if ser.isOpen():
+    ser.close()
+
+ser.open()
+
+ser.write('Test')
+ser.close()
